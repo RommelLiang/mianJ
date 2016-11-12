f7b9dce0c1209e65aeaf2a568f721ab6278a9b25
bearwithcode <bearbearinmel@gmail.com>
2016-11-12 16:49:08 +0800

    new project

---
+++ src/main/java/com/mian/repository/ConsultantRepository.java
@@ -0,0 +1,12 @@
+package com.mian.repository;
+
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+
+/**
+ * Created by victor on 12/11/2016.
+ */
+@Repository
+public interface ConsultantRepository<Consultant,ID extends Serializable>{
+}
