d15225c03d07ed7a4b2218d86ad9d6a70d6326f9
bearwithcode <bearbearinmel@gmail.com>
2016-11-12 10:44:42 +0800

    new project

---
+++ src/main/java/com/mian/controller/ConsultationController.java
@@ -0,0 +1,18 @@
+package com.mian.controller;
+
+import com.mian.bean.Consultant;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by victor on 11/11/2016.
+ */
+@RestController
+public class ConsultationController {
+    @RequestMapping(value = "/ConsultantInfoForm",method = RequestMethod.POST)
+    public @ResponseBody
+    String privateInfoForm(@RequestBody Consultant consultant){
+        String success= "";
+
+        return success;
+    }
+}
--- src/main/java/com/mian/controller/IndexController.java
+++ src/main/java/com/mian/controller/IndexController.java
@@ -1,8 +1,10 @@
 package com.mian.controller;
 
+import org.omg.CORBA.Request;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by victor on 31/10/2016.
@@ -11,6 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @RequestMapping(value="/",method = RequestMethod.GET)
     public String index(){
-        return "你好用户这是面筋，你可以在这找工作,开发人员梁和纳粹的小熊，小熊是我儿子";
+        return "你好用户这是面筋，你可以在这找工作";
+    }
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public ModelAndView hello(){
+        ModelAndView modelAndView = new ModelAndView("hello");
+        System.out.println("Hello should be");
+        return modelAndView;
     }
 }
