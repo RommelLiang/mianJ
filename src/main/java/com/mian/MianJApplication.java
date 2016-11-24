5f245bf5669a042bffdc6beb90e0fdcb82ce7d76
anti-Nazi <lzz931208@yeah.net>
2016-11-24 10:33:17 +0800

    部署成功——Liang

--- src/main/java/com/mian/MianJApplication.java
+++ src/main/java/com/mian/MianJApplication.java
@@ -2,10 +2,15 @@ package com.mian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MianJApplication {
-
+public class MianJApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MianJApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MianJApplication.class, args);
 	}
---
+++ src/test/java/com/mian/controller/EmployerDemandControllerTest.java
@@ -0,0 +1,58 @@
+package com.mian.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * com.mian.controller
+ * Created by Reinhard Tristan Eugen Heydrich
+ * 2016/11/21
+ */
+public class EmployerDemandControllerTest {
+    @Test
+    public void publishPosition() throws Exception {
+
+    }
+
+    @Test
+    public void validateMobile() throws Exception {
+
+    }
+
+    @Test
+    public void checkPublishRecord() throws Exception {
+
+    }
+
+    @Test
+    public void updateDamand() throws Exception {
+
+    }
+
+    @Test
+    public void findSeekers() throws Exception {
+
+    }
+
+    @Test
+    public void arrangeWithSeekers() throws Exception {
+
+    }
+
+    @Test
+    public void findallcomsiltant() throws Exception {
+
+    }
+
+    @Test
+    public void findAllConsultant() throws Exception {
+
+    }
+
+    @Test
+    public void findByCondition() throws Exception {
+
+    }
+
+}
